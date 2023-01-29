7b232dfbf44ece923c8622707353576ccc3c3687
RohitNGiri <procoderpc@gmail.com>
2023-01-29 17:40:29 +0530

    Batman commit my project

---
+++ src/Follower.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Follower = ({avatar_url, html_url, login}) => {
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login}/>
+      <h4>{login}</h4>
+      <a className='btn' href={html_url}>
+      View Profile
+
+      </a>
+    </article>
+  )
+}
+
+export default Follower
